a5b17c04eb9286557008965d8bacfdaa0fc38eb3
Leizhen Shi <frank11157@gmail.com>
2018-08-03 09:05:44 -0400

    Adding tchunk generator

--- src/main/java/FuseJet/POSFormat.java
+++ src/main/java/FuseJet/POSFormat.java
@@ -7,9 +7,12 @@ public class POSFormat {
             //Ready to modify this part so this can fit
             BufferedReader br = new BufferedReader(new FileReader("./test.out.txt"));
 
-            File file = new File("test.pos.txt");
-            file.createNewFile();
-            FileWriter fw = new FileWriter(file);
+            File posfile = new File("test.pos.txt");
+            File tchunkfile = new File("test.tchunk.txt");
+            posfile.createNewFile();
+            tchunkfile.createNewFile();
+            FileWriter posfileWriter = new FileWriter(posfile);
+            FileWriter tchunkfileWriter = new FileWriter(tchunkfile);
 
             String CurrentLine;
 
@@ -20,12 +23,14 @@ public class POSFormat {
                 for (int i = 0; i < lineInString.length; i++){
                     String[] wordProperty = lineInString[i].split("_");
                     long endCounter = counter + wordProperty[0].length() - 1;
-                    fw.write(wordProperty[0] + "  |||  " + "S: " + counter + " " + "E: " + endCounter + "  " + "|||  " + wordProperty[1] + "\n");
+                    posfileWriter.write(wordProperty[0] + "  |||  " + "S: " + counter + " " + "E: " + endCounter + "  " + "|||  " + wordProperty[1] + "\n");
+                    //Missing BIO Tag here
+                    tchunkfileWriter.write(wordProperty[0] + "  " + wordProperty[0] + "  " + wordProperty[1] + "\n");
                     counter = endCounter;
                 }
             }
 
-            fw.close();
+            posfileWriter.close();
         } catch (IOException  e){
             e.printStackTrace();
         }
