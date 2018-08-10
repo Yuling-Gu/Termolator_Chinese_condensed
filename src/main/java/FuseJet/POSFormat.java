e7df21d406d7fbd9bef9b1675992532c2522f702
Leizhen Shi <ls4056@nyu.edu>
2018-08-10 09:24:49 -0400

    Updating tchunker Generator

--- src/main/java/FuseJet/POSFormat.java
+++ src/main/java/FuseJet/POSFormat.java
@@ -1,6 +1,11 @@
 package FuseJet;
 import java.io.*;
 
+/*
+Author: Leizhen Shi
+Update Version Date: 08/10/18
+*/
+
 public class POSFormat {
     public static void main (String[] args){
         try {
@@ -18,14 +23,31 @@ public class POSFormat {
 
             long counter = 1;
 
+            int nounCounter = 0;
+
             while ((CurrentLine = br.readLine()) != null){
                 String[] lineInString = CurrentLine.split(" ");
                 for (int i = 0; i < lineInString.length; i++){
+                    //Generating POS Tag files
                     String[] wordProperty = lineInString[i].split("_");
                     long endCounter = counter + wordProperty[0].length() - 1;
                     posfileWriter.write(wordProperty[0] + "  |||  " + "S: " + counter + " " + "E: " + endCounter + "  " + "|||  " + wordProperty[1] + "\n");
-                    //Missing BIO Tag here
-                    tchunkfileWriter.write(wordProperty[0] + "  " + wordProperty[0] + "  " + wordProperty[1] + "\n");
+                    //Generating tchunk tag files
+                    String nounProperty = wordProperty[1].replaceAll("\\s+","");
+                    if (nounProperty == "NN" || nounProperty == "NP"){ //The program won't enter the if statement here. Something is wrong
+                        if (nounCounter == 0) {
+                            tchunkfileWriter.write(wordProperty[0] + "  " + wordProperty[0] + "  " + wordProperty[1] + "  " + "B" + "\n");
+                            nounCounter ++;
+                        }
+                        else {
+                            tchunkfileWriter.write(wordProperty[0] + "  " + wordProperty[0] + "  " + wordProperty[1] + "  " + "I" + "\n");
+                        }
+                    }
+                    else {
+                        nounCounter = 0;
+                        tchunkfileWriter.write(wordProperty[0] + "  " + wordProperty[0] + "  " + wordProperty[1] + "  " + "O" + "\n");
+
+                    }
                     counter = endCounter;
                 }
             }
